c6f427aa744c21e1b8ce4fe8c5d9c84975ce97af
vincepr <vinceprinceking@gmail.com>
2023-12-26 15:52:24 +0100

    chapter 13 finished

--- 2_neural_networks/ch13.py
+++ 2_neural_networks/ch13.py
@@ -4,6 +4,28 @@ import numpy as np
 ### NEURAL NETWORKS LOGIC:
 ###
 
+### using mini-batch gradient descent instead of GD (gradient descent)
+### - what we used so far is batch GD, because it batches ALL data into one
+###   then runs calculation on that batch
+
+### Notes:
+### - mainly mini-batch-gd gives faster feedback.
+### - because it converges faster during early iterations. So if a whole run takes hours/days
+###     this provides important early feedback about effectiveness of changed hyperparameters etc.
+
+# gets our current batch-slice from the training data
+def prepare_batches(X_train, Y_train, batch_size):
+    x_batches = []
+    y_batches = []
+    n_examples = X_train.shape[0]
+    for batch in range(0, n_examples, batch_size):
+        batch_end = batch + batch_size
+        x_batches.append(X_train[batch:batch_end])
+        y_batches.append(Y_train[batch:batch_end])
+    return x_batches, y_batches
+
+# and we upgraded our train() to use batches instead of all data
+
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
@@ -41,13 +63,12 @@ def loss(Y, y_hat):
     return - np.sum(Y * np.log(y_hat)) / Y.shape[0]
 
 # human readable 'progress-informations'
-def report(iteration, X_train, Y_train, X_test, Y_test, w1, w2):
+def report(epoch, batch, X_train, Y_train, X_test, Y_test, w1, w2):
     y_hat, _ = forward(X_train, w1, w2)
     training_loss = loss(Y_train, y_hat)
     classification = classify(X_test, w1, w2)
     accuracy = np.average(classification == Y_test) * 100.0
-    print("Iteration: %5d, Loss: %.8f, Accuracy: %.2ff%%" % (iteration, training_loss, accuracy))
-
+    print("%5d=epoch|batch=%d > Loss: %.8f, Accuracy: %.2f%%" % (epoch, batch, training_loss, accuracy))
 
 ###
 ### NEURAL NETWORKS TRAINING:
@@ -76,20 +97,30 @@ def initialize_weights(n_input_variables, n_hidden_nodes, n_classes):
     w2 = np.random.randn(w2_rows, n_classes) * np.sqrt(1 / w2_rows)
     return (w1, w2)
 
-def train(X_train, Y_train, X_test, Y_test, n_hidden_nodes, iterations, lr):
+# for mini-batch-gradient-descent:
+# - there are now 2 nested loops.
+#   - the outer loop is called epoch and goes trough all batches of the training set
+#   - the inner loop is gradient descent on a single batch
+# epochs was the old iterations
+# and a more batches will help converge w1, w2 earlier
+def train(X_train, Y_train, X_test, Y_test, n_hidden_nodes, epochs, batch_size, lr):
     n_input_variables = X_train.shape[1]
     n_classes = Y_train.shape[1]
     w1, w2 = initialize_weights(n_input_variables, n_hidden_nodes, n_classes)
-    for i in range(iterations):
-        y_hat, h = forward(X_train, w1, w2)
-        w1_gradient, w2_gradient = back(X_train, Y_train, y_hat, w2, h)
-        w1 = w1 - (w1_gradient * lr)
-        w2 = w2 - (w2_gradient * lr)
-        report(i, X_train, Y_train, X_test, Y_test, w1, w2)
+    x_batches, y_batches = prepare_batches(X_train, Y_train, batch_size)
+    for epoch in range(epochs):
+        for batch in range(len(x_batches)):
+            y_hat, h = forward(x_batches[batch], w1, w2)
+            w1_gradient, w2_gradient = back(x_batches[batch], y_batches[batch], y_hat, w2, h)
+            w1 = w1 - (w1_gradient * lr)
+            w2 = w2 - (w2_gradient * lr)
+            report(epoch, batch, X_train, Y_train, X_test, Y_test, w1, w2)
     return (w1, w2)
 
-import mnist as data
-w1, w2 = train(data.X_train, data.Y_train, data.X_test, data.Y_test, n_hidden_nodes=200, iterations=900, lr=0.01)
-
-
-
+if __name__ == "__main__":
+    import mnist as data
+    w1, w2 = train(data.X_train, data.Y_train, data.X_test, data.Y_test, 
+                   n_hidden_nodes=200, 
+                   epochs=2,
+                   batch_size=256, # with roughly 60000 sets of training data this means 3 batchecs
+                   lr=0.01)
